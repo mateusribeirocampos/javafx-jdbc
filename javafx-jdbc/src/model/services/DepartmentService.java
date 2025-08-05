ea29e752d1f568d828c1767464db52d2504bae09
mateusribeirocampos <mateusrcampos@gmail.com>
2025-08-05 18:36:09 -0300

    chore: Adding database access

--- javafx-jdbc/src/model/services/DepartmentService.java
+++ javafx-jdbc/src/model/services/DepartmentService.java
@@ -1,17 +1,23 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<Department>();
+		
+		return dao.findAll();
+		
+		/*List<Department> list = new ArrayList<Department>();
 		list.add(new Department(1, "Books", 60, "Various titles of major bestsellers"));
 		list.add(new Department(2, "Computers", 15, "Top computer brands"));
 		list.add(new Department(3, "Electronics", 40, "Lots of electronics for your devices"));
-		return list;
+		return list;*/
 	}
 }
